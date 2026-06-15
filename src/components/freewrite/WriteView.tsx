7c58fc106b47a8b15c300bbf75518c21805d4dd0
Nikita D <dnawastaken@gmail.com>
2026-06-15 14:29:07 -0700

    tweaking padding values

--- src/components/freewrite/WriteView.tsx
+++ src/components/freewrite/WriteView.tsx
@@ -11,7 +11,7 @@ export function WriteView({ content, onContentChange }: WriteViewProps) {
       <Textarea
         aria-label='Freewrite'
         autoFocus
-        className='mx-auto h-full max-w-5xl flex-1 resize-none rounded-none border-0 bg-transparent p-0 font-serif text-2xl leading-[1.75] shadow-none focus-visible:border-0 focus-visible:ring-0 sm:text-3xl px-5 pb-24 pt-10 sm:px-10 md:px-16 lg:px-24'
+        className='mx-auto h-full max-w-5xl flex-1 resize-none rounded-none border-0 bg-transparent font-serif leading-[1.75] shadow-none focus-visible:border-0 focus-visible:ring-0 p-5 md:p-12'
         onChange={(event) => onContentChange(event.target.value)}
         placeholder='Just start'
         spellCheck
